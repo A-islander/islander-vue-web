9f1af654fe124dbf2aa8b981c6a6add53e32b98a
A-islander <aislander0@outlook.com>
2022-03-22 18:39:36 +0800

    update postnode render

--- src/components/PostNode.vue
+++ src/components/PostNode.vue
@@ -18,7 +18,7 @@
         </div>
       </template>
       <div class="text item" style="white-space: pre-wrap">
-        <!-- <div v-html="textMiddleware(postNode.value)"></div> -->
+        <!-- <div>{{ postNode.value }}</div> -->
         <PostText :text="postNode.value" />
         <div style="text-align: right; font-size: 10px; color: #63acb5">
           <div @click="sageAdd()">支持SAGE：{{ postNode.sageAddCount }}</div>
@@ -37,10 +37,12 @@
   </div>
 </template>
 <script lang="ts">
+import axios from "axios";
 import { defineComponent, toRefs } from "vue";
 import PostText from "./PostText.vue";
 
 export default defineComponent({
+  name: "PostNode",
   components: {
     PostText,
   },
@@ -48,6 +50,11 @@ export default defineComponent({
     postNode: Object,
   },
   setup(props) {
+    let getPostNode = (postId: number) => {
+      axios.get("forum/get?postId="+postId).then((response) => {
+        console.log(response.data.data)
+      })
+    }
     let sageAdd = () => {
       console.log("ok");
     };
@@ -56,6 +63,7 @@ export default defineComponent({
         .toLocaleString()
         .replace(/:\d{1,2}$/, " ");
     };
+    getPostNode(123)
     return {
       ...toRefs(props),
       sageAdd,
--- src/components/PostText.vue
+++ src/components/PostText.vue
@@ -1,6 +1,52 @@
 
 <script lang="ts">
-import { defineComponent, h } from "vue";
+import { defineComponent, h, toRefs } from "vue";
+
+let textRender = (str: String | undefined) => {
+  if (str === undefined) {
+    str = "";
+  }
+  let urlReg = /(http:\/\/|https:\/\/)((\w|=|\?|\.|\/|&|-)+)|No.[0-9]+/g;
+  let data = str.match(urlReg);
+  let nodeArr = [];
+  let start = 0; // 截取起点
+  let end = 0; // 截取终点
+  let seg = ""; // 截取内容
+  if (data !== null) {
+    // 生成渲染树
+    for (let i = 0; i < data.length; i++) {
+      end = str.slice(start, str.length).indexOf(data[i]);
+      seg = str.slice(start, start + end); // 前段
+      nodeArr.push(seg);
+      if (data[i].indexOf("http") !== -1) {
+        nodeArr.push(
+          h(
+            "a",
+            { href: data[i], style: "color: #63acb5", target: "_blank" },
+            "链接"
+          )
+        );
+      } else if (data[i].indexOf("No.") !== -1) {
+        nodeArr.push(
+          h(
+            "el-button",
+            {
+              onclick: () => {
+                console.log("ok");
+              },
+              style: "color: #63acb5",
+            },
+            data[i]
+          )
+        );
+      }
+      start = start + end + data[i].length;
+    }
+  }
+  nodeArr.push(str.slice(start, str.length));
+  let contanier = h("div", { style: "white-space: pre-wrap" }, nodeArr);
+  return contanier;
+};
 
 export default defineComponent({
   name: "PostText",
@@ -9,54 +55,10 @@ export default defineComponent({
   },
   setup(props) {
     // let str = "2022-03-20日更新\n1. 新增议事厅板块，用以讨论岛民以及岛内务\n2. 新增界面图标，看起来正常一点了\n3. 新增串备份和接口文档地址，方便岛民查询和使用\n4. 更新readme.md，新增搭建教程：http://baidu.com 你好 http://baidu.com http://baidu.com http://baidu.com No.123";
-    let str = props.text;
-    if (str === undefined) {
-      str = "";
-    }
-    let urlReg = /(http:\/\/|https:\/\/)((\w|=|\?|\.|\/|&|-)+)|No.[0-9]+/g;
-    let data = str.match(urlReg);
-    let nodeArr = [];
-    let start = 0; // 截取起点
-    let end = 0; // 截取终点
-    let seg = ""; // 截取内容
-    if (data !== null) { // 生成渲染树
-      for (let i = 0; i < data.length; i++) {
-        end = str.slice(start, str.length).indexOf(data[i]);
-        seg = str.slice(start, start + end); // 前段
-        nodeArr.push(seg);
-        if (data[i].indexOf("http") !== -1) {
-          nodeArr.push(
-            h(
-              "a",
-              { href: data[i], style: "color: #63acb5", target: "_blank" },
-              "链接"
-            )
-          );
-        } else if (data[i].indexOf("No.") !== -1) {
-          nodeArr.push(
-            h(
-              "el-button",
-              {
-                onclick: () => {
-                  console.log("ok");
-                },
-                style: "color: #63acb5",
-              },
-              data[i]
-            )
-          );
-        }
-        start = start + end + data[i].length;
-      }
-    }
-    nodeArr.push(str.slice(start, str.length));
-    let contanier = h("div", { style: "white-space: pre-wrap" }, nodeArr);
-    return {
-      contanier,
-    };
+    return { ...toRefs(props) };
   },
-  render() {
-    return this.contanier;
+  render() { // 在渲染中执行函数，setup在启动后便无法返回更新的text数据
+    return textRender(this.text);
   },
 });
 </script>
